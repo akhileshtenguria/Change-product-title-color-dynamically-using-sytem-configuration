3aeb65bd7d49a1ca6c00fe554a03625bfc12dd79
akhileshtenguria <79194761+akhileshtenguria@users.noreply.github.com>
2022-10-07 11:35:36 +0530

    product title color change
    product title color change using store configuration setting.

---
+++ ColorPicker/Helper/Data.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Nitesh\ColorPicker\Helper;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+class Data extends AbstractHelper
+{
+    const XML_PATH_MODULE_CONFIG = 'changecolor/general/my_color_option';
+
+    public $_storeManager;
+    public function __construct(
+        StoreManagerInterface $storeManager,
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->_storeManager = $storeManager;
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    public function getConfigValue($field, $storeId = null)
+    {
+        return $this->scopeConfig->getValue(
+            $field,
+            ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
+
+    public function getConfig($storeId = null)
+    {
+        return $this->getConfigValue(self::XML_PATH_MODULE_CONFIG);
+    }
+}
